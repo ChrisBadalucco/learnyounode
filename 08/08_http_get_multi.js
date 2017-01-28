d9f876109007a0425289e76ac017ca39b079e1a4
Chris Badalucco <cbadalucco@gmail.com>
2017-01-28 01:13:19 -0500

    exercise 8 - passing

---
+++ 08/08_http_get_multi.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var url = process.argv[2],
+    encodedUrl = encodeURI(url),
+    dataStr = '';
+
+http.get(encodedUrl, function(response) {
+    response.setEncoding('utf8'); // intellij isn't happy. doesn't know setEncoding is available on response
+    response.on('data', function(data) {
+        dataStr += data;
+    }).on('end', function() {
+        console.log(dataStr.length);
+        console.log(dataStr);
+    });
+});
