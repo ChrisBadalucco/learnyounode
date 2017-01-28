c5b3f85de0194a8dfde1e3afbbf52f90249e48b3
Chris Badalucco <cbadalucco@gmail.com>
2017-01-28 01:12:58 -0500

    formatting

--- 07/07_http_get.js
+++ 07/07_http_get.js
@@ -1,11 +1,11 @@
 var http = require('http');
 
 var url = process.argv[2],
-encodedUrl = encodeURI(url);
+    encodedUrl = encodeURI(url);
 
 http.get(encodedUrl, function(response) {
     response.setEncoding('utf8'); // intellij isn't happy. doesn't know setEncoding is available on response
     response.on('data', function(data) {
         console.log(data);
-    })
+    });
 });
