94c976c94e20de9b4731be3f127d04599428220a
Chris Badalucco <cbadalucco@gmail.com>
2017-01-30 22:40:04 -0500

    added error handling

--- 08/08_http_collect.js
+++ 08/08_http_collect.js
@@ -6,10 +6,12 @@ var url = process.argv[2],
 
 http.get(encodedUrl, function(response) {
     response.setEncoding('utf8'); // intellij isn't happy. doesn't know setEncoding is available on response
-    response.on('data', function(data) {
-        dataStr += data;
+    response.on('data', function(chunk) {
+        dataStr += chunk;
     }).on('end', function() {
         console.log(dataStr.length);
         console.log(dataStr);
+    }).on('error', function(error) {
+        console.error(error);
     });
 });
