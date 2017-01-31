fe657060ac4e1f3d71e3ba039d2b3f05a26fe62c
Chris Badalucco <cbadalucco@gmail.com>
2017-01-30 22:41:44 -0500

    attempt to cleanup code

--- 08/08_http_collect_thirdparty.js
+++ 08/08_http_collect_thirdparty.js
@@ -5,10 +5,13 @@ var url = process.argv[2],
     encodedUrl = encodeURI(url);
 
 http.get(encodedUrl, function(response) {
-    response.pipe(concat(function(data) {
-        console.log(data.toString().length);
-        console.log(data.toString());
-    })).on('error', function(error) {
+    response.on('error', function(error) {
         console.error(error);
     });
+
+    var write = concat(function(data) {
+        console.log(data.toString().length);
+        console.log(data.toString());
+    });
+    response.pipe(write);
 });
